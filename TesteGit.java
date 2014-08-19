064bdd0bab3058ab9f65529fd64ba8dc62bb0125
Felipe <felipe.nascimento0204@live.com>
2014-08-18 22:21:11 -0300

    Commit TesteGit

--- TesteGit.java
+++ TesteGit.java
@@ -1,6 +1,6 @@
 public class TesteGit {
 
 	public static void main(String[] args) {
-		System.out.println("Exemplo de teste de uso git");
+		System.out.println("Exemplo de teste de uso git 2");
 	}
 }
